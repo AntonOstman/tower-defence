5c60a008d632f1856e6614b90569739755dd0bf0
anton <antos931@student.liu.se>
2021-04-15 18:27:04 +0200

    Fixed inspections in MapViewer Added names to variables in MapViewer

--- src/se/liu/antos931jakos322/towerdefence/MapViewer.java
+++ src/se/liu/antos931jakos322/towerdefence/MapViewer.java
@@ -16,14 +16,11 @@ public class MapViewer
 {
     private Map map;
     private JFrame frame;
-    private JComboBox towerDropDown;
-    private MapComponent mapComponent;
 
     //
     public MapViewer(Map map) {
         this.map = map;
         this.frame = new JFrame();
-	this.mapComponent = new MapComponent(map);
     }
 
     public void viewMapText(){
@@ -38,33 +35,29 @@ public class MapViewer
 }
     public void show(){
 	MapComponent mapComponent = new MapComponent(map);
-	MapComponent mapComponent1 = new MapComponent(map);
-	//System.out.println(map);
 	JPanel mainPanel = new JPanel();
-	//mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
 	JPanel gamePanel = new JPanel(new GridLayout(0,1));
 	JPanel menuPanel = new JPanel(new GridLayout(4,0));
-	//show borders for debuggiung
+
 	menuPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	gamePanel.setBorder(BorderFactory.createLineBorder(Color.RED));
 	mainPanel.setBorder(BorderFactory.createLineBorder(Color.GREEN));
 
 	String[] towerStrings = {" ","Arrow tower", "Cannon tower", "Money tower"};
 
-	JComboBox towerDropDown = new JComboBox(towerStrings);
+	JComboBox<String> towerDropDown = new JComboBox<>(towerStrings);
 	towerDropDown.setSelectedIndex(0);
-	//towerDropDown.addActionListener();
 
 	//menupanel
 	menuPanel.add(new Button("tester"));
 	menuPanel.add(new Button("tester2"));
 	menuPanel.add(towerDropDown);
-	//gamepalen
+
+	//gamepanel
 	gamePanel.add(mapComponent);
 	MouseEvent mouseListener = new MouseEvent(towerDropDown);
 	gamePanel.addMouseListener(mouseListener);
-	//menuPanel.add(mapComponent1);
 
 	mainPanel.add(gamePanel);
 	mainPanel.add(menuPanel);
@@ -72,10 +65,8 @@ public class MapViewer
 
 	frame.setLayout(new BorderLayout());
 	map.addListener(mapComponent);
-	map.addListener(mapComponent1);
 	frame.add(mainPanel);
 
-	//frame.add(mapComponent, BorderLayout.CENTER);
 	frame.pack();
 	frame.setVisible(true);
 
@@ -93,13 +84,17 @@ public class MapViewer
 	    System.out.println(e);
 	    //System.out.println(e.getLocationOnScreen());
 	    Point clickedPoint = e.getPoint();
+	    // we get the tilesize and translate from pixel coordinates to map coordinates
 	    int tileSize = MapComponent.getTileSize();
-	    Point blockPoint = new Point(clickedPoint.x/tileSize,clickedPoint.y/tileSize);
+	    int mapPosX = clickedPoint.x/tileSize;
+	    int mapPosY = clickedPoint.y/tileSize;
+
+	    Point blockPoint = new Point(mapPosX,mapPosY);
 
 	    System.out.println(e.getPoint() + " Point");
 	    String currentSelectedTower = (String) dropDown.getSelectedItem();
 
-	    if (currentSelectedTower == "Arrow tower"){
+	    if (currentSelectedTower.equals("Arrow tower")){
 	        map.addTower(new ArrowTower(blockPoint));
 	    }
 	}
