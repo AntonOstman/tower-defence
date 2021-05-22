d9ed1a8b78ae2d0609dd3c6d8fbdbd7f74527354
anton <antos931@student.liu.se>
2021-05-22 15:46:24 +0200

    changed how th code is positioned a bit in exploding enemies

--- src/se/liu/antos931jakos322/towerdefence/entities/enemies/ExplodingEnemy.java
+++ src/se/liu/antos931jakos322/towerdefence/entities/enemies/ExplodingEnemy.java
@@ -24,20 +24,23 @@ public class ExplodingEnemy extends Enemy
     @Override public List<Enemy> split(){
 	List<Enemy> enemies = new ArrayList<>();
 	SpeedEnemy s1 = new SpeedEnemy();
-	s1.setPosition(new Point2D.Double(position.getX()+randomPos(), position.getY()+randomPos()));
-	s1.setPathProgress(pathProgress);
-	enemies.add(s1);
 	SpeedEnemy s2 = new SpeedEnemy();
-	s2.setPosition(new Point2D.Double(position.getX()+randomPos(), position.getY()+randomPos()));
-	s2.setPathProgress(pathProgress);
-	enemies.add(s2);
 	SpeedEnemy s3 = new SpeedEnemy();
-	s3.setPosition(new Point2D.Double(position.getX()+randomPos(), position.getY()+randomPos()));
-	s3.setPathProgress(pathProgress);
-	enemies.add(s3);
 	SpeedEnemy s4 = new SpeedEnemy();
+
+	s1.setPosition(new Point2D.Double(position.getX()+randomPos(), position.getY()+randomPos()));
+	s2.setPosition(new Point2D.Double(position.getX()+randomPos(), position.getY()+randomPos()));
+	s3.setPosition(new Point2D.Double(position.getX()+randomPos(), position.getY()+randomPos()));
 	s4.setPosition(new Point2D.Double(position.getX()+randomPos(), position.getY()+randomPos()));
+
+	s1.setPathProgress(pathProgress);
+	s2.setPathProgress(pathProgress);
+	s3.setPathProgress(pathProgress);
 	s4.setPathProgress(pathProgress);
+
+	enemies.add(s1);
+	enemies.add(s2);
+	enemies.add(s3);
 	enemies.add(s4);
 
 	return enemies;
